b39f49e8e8b239092597ce1ba451701e9544b89d
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-08-27 14:03:02 +0900

    Simplify TypedEvent

--- src/TypedEvent.ts
+++ src/TypedEvent.ts
@@ -8,10 +8,8 @@ export interface Disposable {
   dispose(): void;
 }
 
-/** passes through events as they happen. You will not get events from before you start listening */
 export class TypedEvent<T = EventArgs> {
   private listeners: Listener<T>[] = [];
-  private listenersOncer: Listener<T>[] = [];
 
   on = (listener: Listener<T>): Disposable => {
     this.listeners.push(listener);
@@ -21,22 +19,15 @@ export class TypedEvent<T = EventArgs> {
     };
   };
 
-  once = (listener: Listener<T>): void => {
-    this.listenersOncer.push(listener);
-  };
-
   off = (listener: Listener<T>): void => {
     const callbackIndex = this.listeners.indexOf(listener);
     if (callbackIndex > -1) this.listeners.splice(callbackIndex, 1);
   };
 
   emit = (event: T): void => {
-    /** Update any general listeners */
-    this.listeners.forEach(listener => listener(event));
-
-    /** Clear the `once` queue */
-    this.listenersOncer.forEach(listener => listener(event));
-    this.listenersOncer = [];
+    for (const listener of this.listeners) {
+      listener(event);
+    }
   };
 
   pipe = (te: TypedEvent<T>): Disposable => {
