b7885eda0da205197522300a3c8a9365acbb50f8
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-08-29 14:37:18 +0900

    Add vuejs-demo

---
+++ vuejs-demo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{message}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: "App",
+  components: {
+  },
+  setup() {
+    const message = ref<string>('hello');
+
+    return {
+      message,
+    };
+  }
+});
+</script>
