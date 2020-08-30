3265a04280a9659bc0e940b71a61fce21064e863
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2020-08-30 14:58:22 +0900

    Add vuejs-demo

--- vuejs-demo/src/App.vue
+++ vuejs-demo/src/App.vue
@@ -1,21 +1,49 @@
 <template>
   <div id="app">
-    {{message}}
+    {{testModel.valueA}}
+    <br/>
+    (undo:{{history.undoRedoCount[0]}}, redo:{{history.undoRedoCount[1]}})
+    <br/>
+
+    <button @click="undo">undo</button>
+    <button @click="redo">redo</button>
+
+    <button @click="inc">inc</button>
+    <button @click="dec">dec</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref } from '@vue/composition-api';
+import { TestModel } from './models/TestModel';
+import { History } from '../../lib/src/History';
 
 export default defineComponent({
   name: "App",
   components: {
   },
   setup() {
-    const message = ref<string>('hello');
+    const _history = new History();
+    const _testModel = new TestModel(_history);
+
+    const history = ref(_history);
+    const testModel = ref(_testModel);
+    const valueA = ref(_testModel.valueA);
+
+    const undo = () => _history.undo();
+    const redo = () => _history.redo();
+    const inc = () => ++ _testModel.valueA;
+    const dec = () => -- _testModel.valueA;
 
     return {
-      message,
+      history,
+      testModel,
+      valueA,
+
+      undo,
+      redo,
+      inc,
+      dec
     };
   }
 });
